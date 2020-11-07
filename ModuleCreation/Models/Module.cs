f2336039d8a62c13c84a4f4ced47af28c3c0925a
Zairul Mazwan <zairulmazwan@gmail.com>
2020-11-07 21:01:00 +0000

    Add project files.

---
+++ ModuleCreation/Models/Module.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuleCreation.Models
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Module Code")]
+        public string ModuleCode { get; set; }
+        [Required]
+        [Display(Name = "Module Name")]
+        public string ModuleName { get; set; }
+        [Required]
+        public int ModuleLevel { get; set; }
+
+        [Required]
+        public List<bool> ModuleYear { get; set; } = new List<bool> { false, false, false, false }; //We will have year 1,2,3,4 : using checkboxes - True of flase
+        [Required]
+        public List<string> ModuleCourse { get; set; }
+        [Required]
+        public string ModuleOfferStatus { get; set; }
+
+    }
+}
